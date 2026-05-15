a68aa8b2511aab02cd5844bf38d1db058375b0d6
MiguelSanchez-2114 <miguel.sanchez4082@alumnos.udg.mx>
2026-05-14 19:13:53 -0600

    feat(controller/chat): Creacion de controlador para conversaciones y metodo para agregar mensaje

--- src/chat_rag/controllers/chat.py
+++ src/chat_rag/controllers/chat.py
@@ -1,15 +1,39 @@
 from chat_rag.controllers.autorizacion import Autorizacion
 from chat_rag.db.models.mensaje_model import MensajeModel
-
+from chat_rag.controllers.conversacion import Conversacion
+from chat_rag.config import config
 
 class Chat:
-    auth: Autorizacion = None
+    sesion: Autorizacion = None
+    __conversacion: Conversacion = None
     def __init__(self):
-        self.auth = Autorizacion()
+        self.sesion = Autorizacion()
+
+    @property
+    def conversacion(self) -> Conversacion:
+        if self.__conversacion is None:
+            self.__iniciar_conversacion()
+        return self.__conversacion
 
     def obtener_ultimos_mensajes(self):
-        usuario_actual = self.auth.usuario_actual
+        usuario_actual = self.sesion.usuario_actual
         if usuario_actual is None:
             raise ValueError("No hay un usuario autenticado.")
-        mensajes = MensajeModel.obtener_ultimos_mensajes(self.auth.usuario_actual.id)
+        mensajes = MensajeModel.obtener_ultimos_mensajes(self.sesion.usuario_actual.id)
         return mensajes
+    
+    def agregar_mensaje(self, contenido: str, emisor: str):
+        try:
+            return self.conversacion.agregar_mensaje(contenido, emisor)
+        except Exception as e:
+            raise ValueError(f"Error al agregar mensaje: {e}")
+
+    def __iniciar_conversacion(self):
+        if self.sesion.usuario_actual is None:
+            raise ValueError("No hay un usuario autenticado.")
+        if self.__conversacion is None:
+            try:
+                self.__conversacion = Conversacion(id_usuario=self.sesion.usuario_actual.id)
+                self.__conversacion.iniciar_conversacion()
+            except Exception as e:
+                raise ValueError(f"Error al iniciar conversación: {e}")
